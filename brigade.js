384f2973220aa72131d8db87a5471fef86de7bb2
Kshitiz Sharma <kshitiz.sharma@lapr137.successivetech.com>
2019-11-28 13:17:12 +0530

    newov

---
+++ brigade.js
@@ -0,0 +1,16 @@
+#const { events, Job } = require("brigadier");
+#events.on("exec", () => {
+#  var job = new Job("do-nothing", "alpine:3.8");
+#  job.tasks = [
+#    "echo Hello",
+#    "echo World"
+#  ];
+#
+#  job.run();
+#});
+
+const { events } = require("brigadier");
+
+events.on("push", function(e, project) {
+  console.log("received push for commit " + e.revision.commit)
+})
