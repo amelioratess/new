b0bf1b3c3ca13898a2c8be110ac0c8f56aaa534a
Kshitiz Sharma <kshitiz.sharma@lapr137.successivetech.com>
2019-11-29 13:57:12 +0530

    soso

--- brigade.js
+++ brigade.js
@@ -1,5 +1,18 @@
-const { events, Job, Group } = require("brigadier");
+#const { events, Job, Group } = require("brigadier");
 
-events.on("push", function(e, amelioratess/new) {
-  console.log("Hello world");
-})
+#events.on("push", function(e, amelioratess/new) {
+#  console.log("Hello world");
+#})
+
+
+
+const { events, Job } = require("brigadier");
+events.on("exec", () => {
+  var job = new Job("do-nothing", "alpine:3.8");
+  job.tasks = [
+    "echo Hello",
+    "echo World"
+  ];
+
+  job.run();
+});
