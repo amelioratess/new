37a26a87b49e2c0dff27ba5c636c4b5760d9c557
Kshitiz Sharma <kshitiz.sharma@lapr137.successivetech.com>
2019-11-29 14:36:47 +0530

    121

--- brigade.js
+++ brigade.js
@@ -1,8 +1,8 @@
-#const { events, Job, Group } = require("brigadier");
-
+/*#const { events, Job, Group } = require("brigadier");
+#
 #events.on("push", function(e, amelioratess/new) {
 #  console.log("Hello world");
-#})
+#})*/
 
 
 
