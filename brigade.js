4215a53e3a4c867037c670598d569a09bb73e1d6
Kshitiz Sharma <kshitiz.sharma@lapr137.successivetech.com>
2019-12-02 13:59:13 +0530

    --

--- brigade.js
+++ brigade.js
@@ -18,7 +18,7 @@ events.on("push", () => {
     "ls",
     "docker login -u kshitizsh12 -p 10sharma10",
     "echo docker hub auth",
-    "docker build -t kshitizsh12/node-app-ksh .",
+    "docker build -t kshitizsh12/node-app-ksh:2.1 .",
     "echo build successful",
     "docker tag kshitizsh12/node-app-ksh:2.1 kshitizsh12/node-app-ksh:3",
     "echo docker tagging",
