7a9fc1538ab6726bd289ae269c8d89faee8c5320
Kshitiz Sharma <kshitiz.sharma@lapr137.successivetech.com>
2019-12-04 13:27:39 +0530

    forwarding

--- brigade.js
+++ brigade.js
@@ -8,7 +8,7 @@
 
 const { events, Job } = require("brigadier");
 events.on("push", () => {
-  var job = new Job("job1", "alpine-dind:latest");
+  var job = new Job("job1", "fgrehm/alpine-dind:latest");
   job.privileged = true;
   job.tasks = [
     "dockerd &",
