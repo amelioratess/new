75de337391cd1ba8b02fd24ce0cc36e97419fe52
Kshitiz Sharma <kshitiz.sharma@lapr137.successivetech.com>
2020-01-21 13:16:00 +0530

    mayursss

--- brigade.js
+++ brigade.js
@@ -1,32 +1,107 @@
+
+const { events, Job } = require("brigadier");
+events.on("push", () => {
+  var job = new Job("buildfinal", "docker:dind");
+  job.privileged = true;
+//  job.storage.enabled = true;
+  job.env = {
+    DOCKER_DRIVER: "overlay",
+  }
+  //  job.allowHostMounts = true;
+  job.tasks = [
+      "cd /src",
+      "ls -l",
+      "dockerd-entrypoint.sh &",
+      "sleep 20",
+      "docker ps"
+   // "echo 'u r in root'",
+   // "apt-get update",
+    //"apt-get install -y docker",
+    //"echo docker installed",
+    //"cd /etc/init.d/",
+    "ls -l",
+    //   "./etc/init.d/docker start",
+    "./usr/bin/docker start",
+    "sleep 10",
+    //    "cd /etc/init.d/",
+    //    "ls -l",
+    "cd /src/image-processing",
+    "ls -l",
+    "docker login -u mayursuccessive -p Successive@123",
+    "echo docker login done",
+    //"docker build -t mayursuccessive/packageimage:latest .",
+    //"echo docker build done",
+    "docker images",
+    "docker ps"
+
+  ];
+
+
+  job.run();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*#const { events, Job, Group } = require("brigadier");
 #
 #events.on("push", function(e, amelioratess/new) {
 #  console.log("Hello world");
 #})*/
-const { events, Job } = require("brigadier");
-events.on("push", async () => {
-  var job = new Job("job1", "kshitizsh12/base:v1");
-  job.privileged = true;
-  job.tasks = [
-    "dockerd &",
-    "sleep 5",
-    "cd /src",
-    "echo inside project repo",
-    "ls",
-    "docker login -u kshitizsh12 -p 10sharma10",
-    "echo docker hub auth",
-  //  "docker build -t kshitizsh12/node-app-ksh:2.1 .",
-    "echo build successful",
-  //  "docker tag kshitizsh12/node-app-ksh:2.1 kshitizsh12/node-app-ksh:3",
-    "echo docker tagging",
-  //  "docker push kshitizsh12/node-app-ksh:3",
-    "echo docker push"
-  ];
+
+
+
+// const { events, Job } = require("brigadier");
+// events.on("push", async () => {
+//   var job = new Job("job1", "kshitizsh12/base:v1");
+//   job.privileged = true;
+//   job.tasks = [
+//     "dockerd &",
+//     "sleep 5",
+//     "cd /src",
+//     "echo inside project repo",
+//     "ls",
+//     "docker login -u kshitizsh12 -p 10sharma10",
+//     "echo docker hub auth",
+//   //  "docker build -t kshitizsh12/node-app-ksh:2.1 .",
+//     "echo build successful",
+//   //  "docker tag kshitizsh12/node-app-ksh:2.1 kshitizsh12/node-app-ksh:3",
+//     "echo docker tagging",
+//   //  "docker push kshitizsh12/node-app-ksh:3",
+//     "echo docker push"
+//   ];
   
-   
-  const data = await job.run();
-  console.log(data);
-});
+
+
+
+
+//   const data = await job.run();
+//   console.log(data);
+// });
  
 
 
