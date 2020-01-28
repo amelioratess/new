d4725ca090e1b3389ec0b6517dd87c0504b8f146
Kshitiz Sharma <kshitiz.sharma@lapr137.successivetech.com>
2020-01-28 17:02:48 +0530

    new nfs

--- brigade.js
+++ brigade.js
@@ -3,10 +3,12 @@
 #events.on("push", function(e, amelioratess/new) {
 #  console.log("Hello world");
 #})*/
-
-const { events, Job } = require("brigadier");
+const { events, Job, Group } = require("brigadier");
 events.on("push", async () => {
+  var dest = "/mnt/brigade/share/1.txt";
+
   var job = new Job("job1", "kshitizsh12/base:v1");
+  job.storage.enabled = true;
   job.privileged = true;
   job.tasks = [
     "dockerd &",
@@ -15,7 +17,7 @@ events.on("push", async () => {
     "echo inside project repo",
     "ls",
     "docker login -u kshitizsh12 -p 10sharma10",
-    "echo docker hub auth",
+    "echo docker hub auth >" + dest, 
   //  "docker build -t kshitizsh12/node-app-ksh:2.1 .",
     "echo build successful",
   //  "docker tag kshitizsh12/node-app-ksh:2.1 kshitizsh12/node-app-ksh:3",
@@ -24,15 +26,26 @@ events.on("push", async () => {
     "echo docker push",
     "docker ps"
   ];
-  
+
+ var joby = new Job("job2", "docker:dind");
+ joby.storage.enabled = true;
+ job.privileged = true;
+ joby.tasks = [
+  "dockerd &",
+  "sleep 5",
+  "cat " + dest;
+  ]
 
 
+//  const data = await job.run();
+//  console.log(data);
+// above two line for getting logs in brigade worker
 
+Group.runEach([job, joby])
 
-  const data = await job.run();
-  console.log(data);
 });
  
 
 
 // for test
+
